f36e1c2c161821b0e0578c962496c33b2520ff7f
Milen Khanal <milenkhanal@gmail.com>
2019-08-04 18:21:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,8 +24,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(arrays, arrays)
-  arrays.concat([arrays])
+def using_concat(arrays, array)
+  arrays.concat([array])
 end
   
   
